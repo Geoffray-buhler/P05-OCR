aba7b2eb05dcb508472e0978953153d745c551ee
geoffray-buhler <geoffray.buhler@gmail.com>
2021-09-28 13:13:11 +0200

    Modification des ' par " dans le JS

--- site/public/js/main.js
+++ site/public/js/main.js
@@ -1,10 +1,10 @@
-const btn = document.querySelector('.btn-hamburger');
-const div = document.querySelector('#hamburger');
+const btn = document.querySelector(".btn-hamburger");
+const div = document.querySelector("#hamburger");
 
-btn.addEventListener('click',(e)=>{
-  if(div.classList.contains('d-none')){
-      div.classList.remove('d-none')
+btn.addEventListener("click",(e)=>{
+  if(div.classList.contains("d-none")){
+      div.classList.remove("d-none")
   }else{
-      div.classList.add('d-none')
+      div.classList.add("d-none")
   }
 })
